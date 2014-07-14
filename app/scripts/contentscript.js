4d312eb4a0dcf893c8c181d1c838365e582f744b
Mr_Mig <alexey.migutsky@gmail.com>
2014-07-14 15:20:54 +0300

    Small tuning

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,53 +1,47 @@
 'use strict';
 
-var btnId = 'btnCommentsSorter';
+var btnId = '#btnCommentsSorter';
 var btnMessage = 'Отсортировать комментарии';
-var expandCommentsLinksSelector = 'a.expand-thread';
+var collapsedCommentsSelector = '.ct-hidden';
 var commentsSelector = '[class*="b-comment level-"]';
+var threadCommentsLingSelector = '.thread-comments';
+var desc = true;
+var initialStateChanged = false;
 
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
-// Code copied from: http://davidwalsh.name/javascript-debounce-function
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        }, wait);
-        if (immediate && !timeout) func.apply(context, args);
-    };
-}
+// Good idea is to move this to a separate file
+var tpl = '<a class="btn-comments-sorter btn-comments-sorter-mid" id="' + btnId.substr(1) + '" href="javascript:;"><span>' + btnMessage + '</span></a>'
 
 function sort(desc) {
-    // expand all threads
-    $(expandCommentsLinksSelector).each(function () {
-        this.click();
-    });
-
-    // sort all comments
+    console.time('sort');
     $(commentsSelector).sort(function (first, second) {
+      var firstId = parseInt($(first).find('.comment-link').attr('href').substring(1));
+      var secondId = parseInt($(second).find('.comment-link').attr('href').substring(1));
 
-        var firstId = parseInt($(first).find('.comment-link').attr('href').substring(1));
-        var secondId = parseInt($(second).find('.comment-link').attr('href').substring(1));
-
-        if (!desc) {
-            return (firstId > secondId) ? 1 : (firstId < secondId) ? -1 : 0;
-        }
-        return (firstId > secondId) ? -1 : (firstId < secondId) ? 1 : 0;
+      if (!desc) {
+        return (firstId > secondId) ? 1 : (firstId < secondId) ? -1 : 0;
+      }
+      return (firstId > secondId) ? -1 : (firstId < secondId) ? 1 : 0;
     }).appendTo('#commentsList');
+    console.timeEnd('sort');
 }
 
-function fixIcon(desc, initialStateChanged) {
-    var btnElement = $('#' + btnId);
 
-    if (!initialStateChanged) {
-        btnElement.removeClass('btn-comments-sorter-mid');
-    }
+// this function was a hotspot
+function expandAllThreads(){
+    console.time('expandAll');
+    $(collapsedCommentsSelector).each(function () {
+      this.classList.remove(collapsedCommentsSelector.substr(1));
+    });
+		$(threadCommentsLingSelector).hide();
+    console.timeEnd('expandAll');
+}
+
+function fixIcon(desc) {
+    var btnElement = $(btnId);
+
+    // no need for additional parameter
+    // removeClass will not throw error if there is no class
+    btnElement.removeClass('btn-comments-sorter-mid');
 
     if (desc) {
         btnElement.removeClass('btn-comments-sorter-desc').addClass('btn-comments-sorter-asc');
@@ -56,21 +50,15 @@ function fixIcon(desc, initialStateChanged) {
     }
 }
 
-window.addEventListener('load', function () {
-    var desc = true;
-    var initialStateChanged = false;
+// debounce is not needed as contenscript is loaded after DOM is ready
+// this is extension-specific behavior
+function sortFn() {
+    expandAllThreads();
+    sort(desc);
+    desc = !desc;
+    fixIcon(desc, initialStateChanged);
+    initialStateChanged = true;
+}
 
-    $('div.fixed-menu').append('<a class="btn-comments-sorter btn-comments-sorter-mid" id="' + btnId + '" href="javascript:;"><span>' + btnMessage + '</span></a>');
-
-    var sortFn = debounce(function () {
-        sort(desc);
-        desc = !desc;
-        fixIcon(desc, initialStateChanged);
-        initialStateChanged = true;
-    }, 300);
-
-    var link = document.getElementById(btnId);
-    link.addEventListener('click', function () {
-        sortFn();
-    });
-});
+$('div.fixed-menu').append(tpl);
+$(btnId).on('click', sortFn);
